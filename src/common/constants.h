346b4845dda0d854f2b3e6c558ada4194c4b1a53
Hyblocker <hyblocker@protonmail.com>
2026-09-03 23:56:44 +0200

    chore: bump version

--- src/common/constants.h
+++ src/common/constants.h
@@ -8,7 +8,7 @@ constexpr const char* c_SPACE_CALIBRATOR_STEAM_APP_ID = "3368750";
 constexpr const char* c_STEAMVR_STEAM_APP_ID = "250820";
 
 // @NOTE: you may see this version referred to as Nova across the code-base. Nova was the internal codename for this version of the app.
-#define SPACECAL_VERSION_STRING "v2.0.0-BETA"
+#define SPACECAL_VERSION_STRING "v2.0.1-BETA"
 
 #ifndef M_PI
 #define M_PI 3.1415926535
